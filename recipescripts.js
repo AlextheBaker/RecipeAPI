eb30c83d01bbd5d6c2177260b6c8aed7b79cd0c7
Alex Baker <alexthebaker777@yahoo.com>
2016-12-07 17:27:28 -0600

    addRecipe and newRecipe added

--- recipescripts.js
+++ recipescripts.js
@@ -1,15 +1,40 @@
 $(function (){
 
-	var recipes = $('#recipes');
+	var recipes = $('#recipes');		//an id for storing the recipes that will be inserted into the html later
+	var recipeName = $('#recipeName');		//an id just for recipe name strings
 
-	.ajax({
+	function addRecipe(recipe) {
+		$recipes.append('<li>recipeName: '+ recipe.recipeName'</li>');
+	}
+
+	.ajax({								//API call (needs JSON elements to be specified)
 		type: 'GET',
 		url: '',
 		success: function(recipes) {
 			.each(recipes, function(i, recipe){
-				recipes.append('<li>Recipe Name: '+ recipe.Name '</li>');
+				// recipes.append('<li>Recipe Name: '+ recipe.recipeName '</li>');
+				addRecipe(recipe);
 			});
 		}
+
+	});
+
+	$('#addRecipe').on('click', function()	{
+
+	var recipe = {
+		recipeName: $recipeName.val(),
+	};
+
+	.ajax({
+		type: 'POST',
+		url: '';
+		data: recipe,
+		success: function(newRecipe)	{
+			// recipes.append('<li>Recipe Name: '+ newRecipe.recipeName '</li>');
+			addRecipe(newRecipe);
+		}
+	})
+
 	});
 
 });
